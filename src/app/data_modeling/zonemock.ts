08ff7c9c748ab3294cfa4c001bd7874c88344440
Enrico Moretti <enrico.moretti@digitalvirgo.it>
2018-02-12 15:26:01 +0100

    changed favicon, fixed error in zone command arry declaration

--- src/app/data_modeling/zonemock.ts
+++ src/app/data_modeling/zonemock.ts
@@ -27,6 +27,22 @@ export const ZONECOMMAND2: ZoneCommands[] = [{
   state: 0
 }];
 
+export const ZONECOMMAND3: ZoneCommands[] = [
+  {
+    command: 'R',
+    ifFlag: 0,
+    relatedVnum: 4010,
+    maxExisting: 1,
+    roomVnum: 3080,
+    objVnum1: 0,
+    objVnum2: 0,
+    equipmentPosition: 0,
+    exitNum: 0,
+    state: 0
+  }
+];
+
+
 export const ZONEOBJECT: ZoneObject[] = [
   {
     vnum: 30,
@@ -34,24 +50,16 @@ export const ZONEOBJECT: ZoneObject[] = [
     topRoomNumber: 3099,
     lifeSpan: 15,
     resetMode: 2,
-    zoneCommands: ZONECOMMAND
+    zoneCommands: [ZONECOMMAND2, ZONECOMMAND]
+
   }, {
     vnum: 31,
     zoneName: 'Southern Midgaard Main City~',
     topRoomNumber: 4099,
     lifeSpan: 15,
     resetMode: 2,
-    zoneCommands: ZONECOMMAND2
+    zoneCommands: [ZONECOMMAND]
   }
 ];
 
-export const SINGLEZONEOBJECT: ZoneObject[] = [
-  {
-    vnum: 30,
-    zoneName: 'Northern Midgaard Main City~',
-    topRoomNumber: 3099,
-    lifeSpan: 15,
-    resetMode: 2,
-    zoneCommands: ZONECOMMAND
-  }
-];
+
