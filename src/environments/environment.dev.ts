d0a50639ba5dd1b197e74aa1707bc3ed0fc398a9
Enrico Moretti <enrico.moretti@digitalvirgo.it>
2018-04-26 11:43:25 +0200

    OBJ type, flag and wear position parsed

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
-  zoneRestApi: 'https://gilgamesh.nebbie.it/api/v1/zones',
+  //zoneRestApi: 'https://gilgamesh.nebbie.it/api/v1/zones',
+  zoneRestApi: 'http://mudbe/api/v1/zones',
   zoneDbRestApi: 'http://mudbe/api/v1/dbzones',
   auth: 'mammoletta=gemma'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 
 export const environment = {
   production: false,
-  zoneRestApi: 'https://gilgamesh.nebbie.it/api/v1/zones',
+  zoneRestApi: 'http://mudbe/api/v1/zones',
   zoneDbRestApi: 'http://mudbe/api/v1/dbzones',
   auth: 'mammoletta=gemma'
 };
